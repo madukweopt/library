85a84acb5f9f60b7bf1cb9c19809ffc6141904d8
pascal <madukwepascal@gmail.com>
2022-08-18 22:13:04 +0100

    Create  display form $ return home functions

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const button = document.querySelector('.add-btn');
-const form = document.querySelector('.form')
-const main = document.querySelector('main')
+const form = document.querySelector('.form');
+const main = document.querySelector('main');
+const returnBtn = document.querySelector('.return');
+const submit = document.querySelector('.submit');
+let title = document.querySelector('#title');
+let author = document.querySelector('#author');
+let page = document.querySelector('#pages');
+let myLibrary = [];
 
 
-button.addEventListener('click', function() {
-form.classList.add('form')
-form.classList.remove('hide');
-main.style.display = 'none'
 
-})
+button.addEventListener('click', displayForm)
+returnBtn.addEventListener('click', returnToHome)
+submit.addEventListener('click', getUserInput)
 
-
-let myLibrary = {};
-
-function Book(title, author, pages, read) {
-    this.tittle = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function displayForm() {
+    form.classList.add('form')
+    form.classList.remove('hide');
+    main.style.display = 'none'
 }
+
+function returnToHome() {
+    form.classList.add('hide')
+    main.style.display = 'unset'
+
+}
+
+
 
