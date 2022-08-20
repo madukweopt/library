6a4315a2a6af7dc61da287647942ccd28dbc3631
pascal <madukwepascal@gmail.com>
2022-08-20 01:23:53 +0100

    Display added books in the table

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const tableBody = document.querySelector('#table-body');
 const button = document.querySelector('.add-btn');
 const form = document.querySelector('.form');
 const main = document.querySelector('main');
@@ -18,6 +19,7 @@ function displayForm() {
     form.classList.add('form')
     form.classList.remove('hide');
     main.style.display = 'none'
+    form.title.textContent = ''
 }
 
 function returnToHome() {
@@ -30,21 +32,53 @@ function Book(title, author, pages, comment) {
     this.author = author;
     this.pages = pages;
     this.comment = comment;
-  }v
+  }
   
-  function getUserInput() {
+function getUserInput() {
   let titleText = title.value;
   let authorText = author.value;
   let pageNumber = page.value;
   let commentValue = getReadValue();
   let newBook = new Book(titleText, authorText, pageNumber, commentValue);
   myLibrary.push(newBook);
+  displayArray();
+  title.value = ''
+  author.value = ''
+  page.value = ''
+
   }
   
-  function getReadValue() {
+function getReadValue() {
     if(document.querySelector("input[name='read-book']:checked").value == 'yes') return true;
     else return false;
   }
+
+ function displayArray() {
+  for (let library of myLibrary) {
+    let tableRow = document.createElement('tr');
+    let tableData1 = document.createElement('td');
+    tableData1.textContent = library.title;
+    tableRow.appendChild(tableData1);
+
+    let tableData2 = document.createElement('td');
+    tableData2.textContent = library.author;
+    tableRow.appendChild(tableData2);
+
+    let tableData3 = document.createElement('td');
+    tableData3.textContent = library.pages;
+    tableRow.appendChild(tableData3);
+
+    let tableData4 = document.createElement('td');
+    tableData4.textContent = library.comment;
+    tableRow.appendChild(tableData4);
+
+    tableBody.appendChild(tableRow);
+    form.classList.add('hide');
+  main.style.display = 'initial'
+  }
+
+ } 
+ 
   
 
 
