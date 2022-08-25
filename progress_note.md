63f935389f768e7e5b2e3f8e680af050e50086c6
pascal <madukwepascal@gmail.com>
2022-08-25 09:30:42 +0100

    Modify progress_note.md

---
+++ progress_note.md
@@ -0,0 +1,20 @@
+### What did I learn shortly before the project?
+
+- Organizing Javascript code.
+- Objects and object constructors
+- Prototype
+- Javascript inheritance and the prototype chain
+
+### How have I applied what I learnt?
+
+- Use object constructor to build a mini library.
+- Understand how 'this' behave and apply it.
+- Understand the basic do’s and don’t’s of prototypical inheritance and apply it.
+
+### What challenges did I encounter while building the project?
+
+To grab user inputs and display on the table was challenging.
+
+### How have I improved as a result of the project?
+
+- Basic Proficiency working with Objects
--- script.js
+++ script.js
@@ -4,47 +4,48 @@ const form = document.querySelector('.form');
 const main = document.querySelector('main');
 const returnBtn = document.querySelector('.return');
 const submit = document.querySelector('.submit');
+
 let title = document.querySelector('#title');
 let author = document.querySelector('#author');
 let page = document.querySelector('#pages');
 let myLibrary = [];
 
-addButton.addEventListener('click', displayForm)
-returnBtn.addEventListener('click', returnToTable)
-submit.addEventListener('click', getUserInput)
-submit.addEventListener('click', displayBook)
+addButton.addEventListener('click', displayForm);
+returnBtn.addEventListener('click', returnToTable);
+submit.addEventListener('click', getUserInput);
+submit.addEventListener('click', displayBook);
+
 
 function displayForm() {
-  
-  tableBody.innerHTML = ''
-    form.classList.add('form')
-    form.classList.remove('hide');
-    main.style.display = 'none'
-    returnBtn.style.display = 'initial';
-    clearForm()
+  tableBody.innerHTML = '';
+  form.classList.add('form');
+  form.classList.remove('hide');
+  main.style.display = 'none';
+  returnBtn.style.display = 'initial';
+  clearForm();
   
 }
 
 function clearForm() {
- title.value = ''
-  author.value = ''
-  page.value = ''
-  newBook = ''
+  title.value = '';
+  author.value = '';
+  page.value = '';
+  newBook = '';
 }
 
 function returnToTable() {
-    form.classList.add('hide')
-    main.style.display = 'initial'
-    displayBook()
+  form.classList.add('hide');
+  main.style.display = 'initial';
+  displayBook();
 
 }
 
 // constructor function
-function Book(title, author, pages, comment) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.comment = comment;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
   }
   
   // collects user inputs and sends to myLibrary array of objects
@@ -52,26 +53,26 @@ function getUserInput() {
   let titleText = title.value;
   let authorText = author.value;
   let pageNumber = page.value;
-  let commentValue = getReadValue();
-  let newBook = new Book(titleText, authorText, pageNumber, commentValue);
+  let readValue = getReadValue();
+  let newBook = new Book(titleText, authorText, pageNumber, readValue);
   myLibrary.push(newBook);
-  clearForm()
+  clearForm();
 
   }
   
 function getReadValue() {
-    if(document.querySelector("input[name='read-book']:checked").value == 'yes') return true;
-    else return false;
-  }
+  if(document.querySelector("input[name='read-book']:checked").value == 'yes') return 'YES';
+  else return 'NO';
+}
 
   // displays myLibrary array of objects on the html table
 function displayBook() {
 
-  tableBody.innerHTML = ''
-  main.style.display = 'initial'
+  tableBody.innerHTML = '';
+  main.style.display = 'initial';
   form.classList.add('hide');
-  const bookTable = document.querySelector('tbody')
-  bookTable.innerHTML = ''
+  const bookTable = document.querySelector('tbody');
+  bookTable.innerHTML = '';
   
   
   for (let bookIndex in myLibrary) {
@@ -79,36 +80,38 @@ function displayBook() {
       
       for (let attrIndex in myLibrary[bookIndex]) {
           const cell = document.createElement('td');
-          console.log(cell)
           cell.textContent = myLibrary[bookIndex][attrIndex];
           bookRow.appendChild(cell);  
       }
       // attach edit button to DOM and add eventlistener to it
           const editBtn = document.createElement('td');
-          editBtn.setAttribute('class', 'edit-btn')
+          editBtn.setAttribute('class', 'edit-btn');
           editBtn.textContent = 'Edit';
           editBtn.addEventListener('click', function(ev) {
-          displayForm()
-          returnBtn.style.display = 'none'
-          title.value = myLibrary[bookIndex].title
-          author.value = myLibrary[bookIndex].author
-          page.value = myLibrary[bookIndex].pages
+          displayForm();
+          returnBtn.style.display = 'none';
+          title.value = myLibrary[bookIndex].title;
+          author.value = myLibrary[bookIndex].author;
+          page.value = myLibrary[bookIndex].pages;
           myLibrary.splice(bookIndex, 1);
           
              })
                            
       // append delete button to DOM and add eventlistener to it
           const deleteBtn = document.createElement('td');
-          deleteBtn.setAttribute('class', 'delete-btn')
+          deleteBtn.setAttribute('class', 'delete-btn');
           deleteBtn.textContent = 'Delete'; 
           deleteBtn.addEventListener('click', function() {
           myLibrary.splice(bookIndex, 1);
-          displayBook()
+          displayBook();
           
           })
       bookRow.appendChild(editBtn);
       bookRow.appendChild(deleteBtn);  
-      bookTable.appendChild(bookRow)
+      bookTable.appendChild(bookRow);
  }
 
 }
+
+
+
